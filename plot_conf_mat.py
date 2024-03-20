2bab9e07b29b6961e99b676b9bc8c97e5e82edd0
zjj <75033001+WhateverMO@users.noreply.github.com>
2024-03-20 23:15:07 +0800

    add some utils not test yet

---
+++ plot_conf_mat.py
@@ -0,0 +1,67 @@
+import itertools
+import numpy as np
+# import seaborn as sns
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+
+# 绘制混淆矩阵
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues, name='conf'):
+    """
+,  This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    Input
+    - cm : 计算出的混淆矩阵的值
+    - classes : 混淆矩阵中每一行每一列对应的列
+    - normalize : True:显示百分比, False:显示个数
+    """
+    font = {'family': 'Times New Roman',
+            'weight': 'normal',
+            'size': 14,
+            }
+
+    # 设置输出的图片大小
+    figsize = 8, 6
+    figure, ax = plt.subplots(figsize=figsize)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+    print(cm)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    # plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+    plt.ylabel('True label', font)
+    plt.xlabel('Predicted label', font)
+    # plt.savefig(name + '.png', dpi=300)
+    plt.show()
+
+#
+# cnf_matrix_unsu_m2r = np.array([[16, 0, 0, 0, 0, 1, 3, 1, 1, 4],
+#                              [1, 27, 4, 0, 3, 2, 5, 24, 4, 15],
+#                              [1, 5, 48, 0, 3, 20, 13, 37, 6, 13],
+#                              [0, 0, 18, 0, 2, 11, 51, 48, 0, 19],
+#                              [3, 37, 16, 0, 399, 123, 96, 100, 4, 23],
+#                              [0, 0, 4, 0, 0, 15, 4, 17, 0, 1],
+#                              [0, 0, 16, 0, 0, 2, 33, 8, 0, 2],
+#                              [0, 0, 0, 0, 0, 0, 1, 24, 0, 0],
+#                              [3, 10, 6, 1, 29, 4, 4, 32, 41, 4],
+#                              [5, 16, 18, 2, 1, 14, 22, 64, 16, 143]])
+
+#
+# attack_types = ['Bathtub', 'Bed', 'Bookshelf', 'Cabinet', 'Chair', 'Lamp', 'Monitor', 'Plant', 'Sofa', 'Table',]
+cnf_matrix_unsu_m2r=np.loadtxt(r'D:\Python\KPConv-PyTorch-master\KPConv-PyTorch-master\test_HSCN\A\conf.txt')
+attack_types = ['Powerline', 'Low vegetation', 'surface', 'Car', 'Fence', 'Roof', 'Facade', 'shurb', 'Tree',]
+
+plot_confusion_matrix(cnf_matrix_unsu_m2r, classes=attack_types, normalize=True, title='Normalized confusion matrix', name='2024_confusion_matrix_unsu_m2r')
---
+++ test_accuracy.py
@@ -0,0 +1,120 @@
+import time
+import os
+import sys
+from os.path import join
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import confusion_matrix, f1_score
+from utils.metrics import smooth_metrics,metrics,fast_confusion
+# Custom libs
+
+# Dataset
+from plyfile import PlyData, PlyElement
+labels = ['Gnd', 'Trees', 'Car', 'Truck', 'Wire', 'Fence', 'Poles' , 'Bldngs']
+ignored_labels = ['Unclassified']
+# label_to_names= {0: 'ground',#LASDU
+#                 1: 'building',
+#                 2: 'tree',
+#                 3: 'low_evg',
+#                 4: 'artifact'}
+label_to_names= {0: 'powerline',
+                       1: 'low vegetation',
+                       2: 'impervious surfaces',
+                       3: 'car',
+                       4: 'fence/hedge',
+                       5: 'roof',
+                       6: 'facade',
+                       7: 'shurb',
+                       8: 'tree'
+                               }
+# label_values = np.zeros((0,), dtype=np.int32)
+label_values=np.sort([k for k, v in label_to_names.items()])
+print(len(label_values))
+# final_labels = labels[:]
+# for each in ignored_labels: final_labels.remove(each)
+# Given below is the path to test output and ground truth files.
+# All files in test output file should be present in ground truth folder for successful execution of this file
+# This Also works in windows
+test_predictions_path = r'D:\Python\KPConv-PyTorch-master\KPConv-PyTorch-master\test_output\2024-02-05_12-58-12\val_preds'
+test_groundtruth_path = r'D:\Python\KPConv-PyTorch-master\KPConv-PyTorch-master\test_output\2024-02-05_12-58-12\val_preds'
+files_pred = [f for f in os.listdir(test_predictions_path) if f[-4:] == '.ply']
+files_ground = [f for f in os.listdir(test_groundtruth_path) if f[-4:] == '.ply']
+if(all(each in files_ground for each in files_pred )):
+    print("All files good")
+else:
+    print("Error some files at ",test_predictions_path, "not matching with files at",test_groundtruth_path)
+    exit()
+
+once = False
+total_list_micro = list()
+total_list_macro = list()
+total_list_miou =list()
+acc_list=list()
+Cum = np.zeros((len(label_values), len(label_values)), dtype=np.int32)
+
+ytrue = []
+ypred=[]
+conf_file = join(test_predictions_path, 'conf.txt')
+for c_i,each_file in enumerate(files_pred):
+    print('\n Loading.... ', os.path.join(test_predictions_path, each_file))
+    data_pred = PlyData.read(os.path.join(test_predictions_path, each_file))
+    data_grtr = PlyData.read(os.path.join(test_groundtruth_path, each_file))
+    # y_true = data_grtr.elements[0]['class']
+    # y_pred = data_pred.elements[0]['preds']
+
+    y_true = data_grtr.elements[0]['label'].astype(np.int32)
+    y_pred = data_pred.elements[0]['pred'].astype(np.int32)
+    print(y_true.shape, y_pred.shape)
+    ytrue+=[y_true]
+    ypred+=[y_pred]
+    """ # Uncomment these lines for saving confusion matrix in a color scale in pdf format
+    C = confusion_matrix(y_true, y_pred, normalize='pred')
+    for l_ind, label_value in enumerate(labels):
+        if label_value in ignored_labels:
+            C = np.delete(C, l_ind, axis=0)
+            C = np.delete(C, l_ind, axis=1)
+    if not once:
+        Cum = C
+    else:
+        Cum += C
+    plt.imshow(Cum)
+    ticks = range(len(final_labels))
+    plt.xticks(ticks=ticks,labels=final_labels)
+    plt.yticks(ticks=ticks,labels=final_labels)
+    if not once: plt.colorbar()
+    once = True
+    plt.title(" Confusion Matrix ")
+    plt.savefig("results/"+each_file[:-4]+'.pdf')
+    """
+    # F1_score_macro = f1_score(y_true, y_pred, average='micro')
+    F1_score_macro = f1_score(y_true, y_pred, average='macro')
+    # C = confusion_matrix(y_true, y_pred, normalize='pred')
+    C = fast_confusion(y_true.astype(np.int32), y_pred.astype(np.int32),label_values).astype(np.int32)
+    print(C.shape)
+    Cum+=C
+
+    PRE, REC, F1, IoU, ACC = metrics(C)
+
+    # PRE, REC, F1, IoU, ACC = smooth_metrics(C)
+    print('F1',F1.sum()/9,'ACC',ACC,'AVG.F1',F1.sum()/9)
+
+    print("macro F1: \t",F1_score_macro)
+    print('MIOU: \t',IoU.mean())
+    total_list_miou += [IoU.mean()]
+    total_list_macro += [F1_score_macro]
+
+    acc_list  += [ACC]
+ytrue1=np.concatenate(ytrue,axis=0).astype(np.int32)
+ypred1=np.concatenate(ypred,axis=0).astype(np.int32)
+D=fast_confusion(ytrue1, ypred1,label_values)
+PRE2, REC2, F12, IoU2, ACC2 = metrics(D)
+
+np.savetxt(conf_file, D, '%12d')
+# avg_micro = sum(total_list_micro)/len(total_list_micro)
+
+#
+# avg_macro = sum(total_list_macro)/len(total_list_macro)
+# MIOU=sum(total_list_miou)/len(total_list_miou)
+# ACC=sum(acc_list)/len(acc_list)
+print(  "|  Avg  F1(macro) : ", F12.sum()/len(label_values),"|  Avg  miou : ",IoU2.sum()/len(label_values),"|  ACC : ",ACC2)
