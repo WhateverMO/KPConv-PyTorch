f9e72761f5884d3de94ca03fbb689ba347aea420
zjj <75033001+WhateverMO@users.noreply.github.com>
2024-04-04 18:11:52 +0800

    dev 1

--- train_ISPRS_weak_auto.py
+++ train_ISPRS_weak_auto.py
@@ -8,14 +8,15 @@ from os.path import join
 
 if __name__ == '__main__':
     dataset_path,config = train_ISPRS_weak()
+    max_epoch = str(config.max_epoch)
     log_path = config.saving_path
     log_name = config.saving_path.split('/')[-1]
     dataset_original_path = join(dataset_path,'original_ply')
     plot_convergence(log_path)
-    plot_conf_mat(join(log_path,'val_preds_500','conf.txt'))
-    label_to_color(dataset_original_path,join(log_path,'val_preds_500'))
+    plot_conf_mat(join(log_path,'val_preds_'+max_epoch,'conf.txt'))
+    label_to_color(dataset_original_path,join(log_path,'val_preds_'+max_epoch))
     # plot_convergence(log_path)
-    plot_conf_mat(join(log_path,'teacher_val_preds_500','conf.txt'))
-    label_to_color(dataset_original_path,join(log_path,'teacher_val_preds_500'))
+    plot_conf_mat(join(log_path,'teacher_val_preds_'+max_epoch,'conf.txt'))
+    label_to_color(dataset_original_path,join(log_path,'teacher_val_preds_'+max_epoch))
     test_models(log_path)
     test_accuracy(join('test',log_name,'predictions'),dataset_original_path)
