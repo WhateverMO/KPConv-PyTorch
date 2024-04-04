2ecc20c7af589987fa76957bb166dcafec7b5c1e
zjj <75033001+WhateverMO@users.noreply.github.com>
2024-04-04 19:41:31 +0800

    dev 3

--- tools/logger.py
+++ tools/logger.py
@@ -1,17 +1,23 @@
+import sys
+
 class Logger(object):
     def __init__(self, filename='output.txt'):
         self.terminal = sys.stdout
-        self.log = open(filename, 'w')
+        self.log = open(filename, 'w', buffering=1)
 
     def write(self, message):
         self.terminal.write(message)
         self.log.write(message)
+        self.log.flush()
 
     def flush(self):
         # this flush method is needed for python 3 compatibility.
         # this handles the flush command by doing nothing.
         # you might want to specify some extra behavior here.
         pass
+
+    def __del__(self):
+        self.log.close()
 
 # Redirect stdout
 def redirect_stdout(filename):
