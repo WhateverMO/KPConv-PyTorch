ebbe48eb27de962c25d6b576dbfa1b4b3fd4fc9c
zjj <75033001+WhateverMO@users.noreply.github.com>
2024-05-01 01:43:43 +0800

    change auto seq

--- train_ISPRS_weak_auto.py
+++ train_ISPRS_weak_auto.py
@@ -17,11 +17,6 @@ if __name__ == '__main__':
     plot_convergence(log_name)
     attack_types = ['Powerline', 'Low vegetation', 'surface', 'Car', 'Fence', 'Roof', 'Facade', 'shurb', 'Tree',]
     plot_conf_mat(join(log_path,'val_preds_'+max_epoch,'conf.txt'),attack_types)
-    label_to_color(dataset_original_path,join(log_path,'val_preds_'+max_epoch))
-    # plot_convergence(log_name)
-    # plot_conf_mat(join(log_path,'teacher_val_preds_'+max_epoch,'conf.txt'))
-    label_to_color(dataset_original_path,join(log_path,'teacher_val_preds_'+max_epoch))
-    test_models(log_path)
     label_to_names= {0: 'powerline',
                             1: 'low vegetation',
                             2: 'impervious surfaces',
@@ -33,3 +28,8 @@ if __name__ == '__main__':
                             8: 'tree'
                                 }
     test_accuracy(join('test',log_name,'predictions'),dataset_original_path,label_to_names)
+    test_models(log_path)
+    label_to_color(dataset_original_path,join(log_path,'val_preds_'+max_epoch))
+    # plot_convergence(log_name)
+    # plot_conf_mat(join(log_path,'teacher_val_preds_'+max_epoch,'conf.txt'))
+    label_to_color(dataset_original_path,join(log_path,'teacher_val_preds_'+max_epoch))
